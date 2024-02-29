8f6f47686702dad851e53a21bfe8be19812a0e81
Saanvi Singh <106013819+SaanviSingh12@users.noreply.github.com>
2024-02-29 20:35:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.use('*',function(req,res){
-    res.sendFile(path,join(__dirname,'./client/build/index.html'));
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 })
 
 // User Authentication 
